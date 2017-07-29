9d2d9066ad5afb8192750b3a084a429df9b13c7b
xup6孔明 <xup6>
2017-07-29 22:34:26 +0800

    add acceleraton and left right stop

--- helloworld.py
+++ helloworld.py
@@ -7,10 +7,10 @@ dp = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('hello')
 error = 0
 RED = (255, 0, 0)
-rect1 = pygame.Rect(0, 0, 80, 50)
+rect1 = pygame.Rect(0, 550, 80, 50)
 clk = pygame.time.Clock()
 pygame.key.set_repeat(1, 10)
-
+speed = 0
 # main loop
 while not error:
     clk.tick(60)
@@ -18,12 +18,22 @@ while not error:
         dp.fill((0, 0, 0))
         # key move the rect1
         if ev.type == pygame.KEYDOWN:
+            speed += 1
             if ev.key == K_d:
-                rect1.right += 10
-                rect1.left += 10
+                rect1.right += speed
+                rect1.left += speed
             if ev.key == K_a:
-                rect1.right -= 10
-                rect1.left -= 10
+                rect1.right -= speed
+                rect1.left -= speed
+        if ev.type == pygame.KEYUP:
+            speed = 0
+        if rect1.left < 0:
+            rect1.left = 0
+            rect1.right = 80
+        if rect1.right > 800:
+            rect1.left = 730
+            rect1.right = 800
+
         # draw rect1
         pygame.draw.rect(dp, RED, rect1)
 
